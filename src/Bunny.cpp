0ef2f16090a2ceef0e9139d1b7a26174cca801a0
Zhikai Wang <zhikai@heteroclinic.net>
2015-03-22 09:09:27 -0700

    Added the bareBone project

---
+++ src/Bunny.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Bunny.cpp
+// Author      : Zhikai
+// Version     :
+// Copyright   : (c) 2010 2015 Zhikai Wang/www.heteroclinic.net
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
